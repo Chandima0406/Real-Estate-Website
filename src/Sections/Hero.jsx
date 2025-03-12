2f9dc5edceede8f45a5e2e45f5e9d8fa943b9007
Chandima0406 <chandima0406sajith@gmail.com>
2025-03-12 23:43:55 +0530

    Make hero section

--- src/Sections/Hero.jsx
+++ src/Sections/Hero.jsx
@@ -1,10 +1,30 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useDarkMode } from '../Components/DarkModeContext'
+import heroimg from '../assets/images/hero1.webp'
+import Aos from 'aos'
+import 'aos/dist/aos.css'
 
 const Hero = () => {
+  useEffect(() => {
+    Aos.init({
+      offset: 200,
+      duration: 800,
+      easing: "ease-in-sine",
+      delay: 100,
+    });
+  }, []);
+
+  const {darkMode , toggleDarkMode} = useDarkMode();
+
   return (
-    <div>
-      
+    <>
+    <div className={`${darkMode ? 'dark:bg-black' : 'light bg-white'}`}>
+      <section id='hero' className='w-[95%] h-[600px] m-auto bg-cover bg-center'>
+
+      </section>
+
     </div>
+    </>
   )
 }
 
