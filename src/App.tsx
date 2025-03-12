ff81b5b2d94a63b54f72a6d40cdf0fbc76f87f4a
Chandima0406 <chandima0406sajith@gmail.com>
2025-03-12 22:22:19 +0530

    Make components and Sections

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,22 @@
-import './App.css'
+import React from 'react'
+import {DarkModeProvider} from './Components/DarkModeContext'
+import Header from './Components/Header'
+import Hero from './Sections/Hero'
+import About from './Sections/About'
+import PopularAreas from './Sections/PopularAreas'
+import Properties from './Sections/Properties'
 
-function App() {
-  
 
+const App = () => {
   return (
     <>
-      
-   
+    <DarkModeProvider>
+      <Header/>
+      <Hero/>
+      <About />
+      <PopularAreas />
+      <Properties />
+    </DarkModeProvider>
     </>
   )
 }
